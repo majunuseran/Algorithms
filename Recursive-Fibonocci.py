9d8ecbc2b3793153d7130d3d2e03d23c958d28d3
Majunu Seran <mseran3@gatech.edu>
2019-10-27 16:16:38 -0700

    Recursive-Factorial Function

---
+++ Recursive-Fibonocci.py
@@ -0,0 +1,10 @@
+def fibonacci(index):
+    if index == 0:
+        return 0
+    elif index == 1:
+        return 1
+    else:
+        return fibonacci(index - 1) + fibonacci(index - 2)
+
+
+print("fibonacci series number at index 100 is ..." + str(fibonacci(100)))
